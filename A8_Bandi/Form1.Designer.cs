9560cf8f503ca19e69a99b2cbab014e7f719a9df
Jared Bandi <jbandi@my.dom.edu>
2024-01-16 22:59:02 -0600

    Add project files.

---
+++ A8_Bandi/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace A8_Bandi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrintShows = new System.Windows.Forms.Label();
+            this.lblAnd = new System.Windows.Forms.Label();
+            this.lblTheatre = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.cbTheatre = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblPrintShows
+            // 
+            this.lblPrintShows.AutoSize = true;
+            this.lblPrintShows.Location = new System.Drawing.Point(109, 120);
+            this.lblPrintShows.Name = "lblPrintShows";
+            this.lblPrintShows.Size = new System.Drawing.Size(172, 25);
+            this.lblPrintShows.TabIndex = 0;
+            this.lblPrintShows.Text = "Print Shows From:";
+            // 
+            // lblAnd
+            // 
+            this.lblAnd.AutoSize = true;
+            this.lblAnd.Location = new System.Drawing.Point(233, 192);
+            this.lblAnd.Name = "lblAnd";
+            this.lblAnd.Size = new System.Drawing.Size(48, 25);
+            this.lblAnd.TabIndex = 1;
+            this.lblAnd.Text = "And";
+            // 
+            // lblTheatre
+            // 
+            this.lblTheatre.AutoSize = true;
+            this.lblTheatre.Location = new System.Drawing.Point(177, 259);
+            this.lblTheatre.Name = "lblTheatre";
+            this.lblTheatre.Size = new System.Drawing.Size(104, 25);
+            this.lblTheatre.TabIndex = 2;
+            this.lblTheatre.Text = "At Theatre";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(300, 314);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(242, 100);
+            this.btnExcel.TabIndex = 3;
+            this.btnExcel.Text = "Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(328, 116);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(214, 29);
+            this.dtpStart.TabIndex = 4;
+            this.dtpStart.Value = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(328, 188);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(214, 29);
+            this.dtpEnd.TabIndex = 5;
+            this.dtpEnd.Value = new System.DateTime(2020, 1, 1, 0, 0, 0, 0);
+            // 
+            // cbTheatre
+            // 
+            this.cbTheatre.FormattingEnabled = true;
+            this.cbTheatre.Location = new System.Drawing.Point(338, 259);
+            this.cbTheatre.Name = "cbTheatre";
+            this.cbTheatre.Size = new System.Drawing.Size(204, 32);
+            this.cbTheatre.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbTheatre);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.lblTheatre);
+            this.Controls.Add(this.lblAnd);
+            this.Controls.Add(this.lblPrintShows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrintShows;
+        private System.Windows.Forms.Label lblAnd;
+        private System.Windows.Forms.Label lblTheatre;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.ComboBox cbTheatre;
+    }
+}
+
---
+++ A8_Bandi/Form1.cs
@@ -0,0 +1,102 @@
+﻿//Jared Bandi
+//Program 8 
+// This program searches an array and prints out a report in excel about broadway shows in Chicago.
+using Shows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace A8_Bandi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Creates a list to read in the array of theatres
+        List<String> TheaterNames = new List<String>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TheaterNames.Add("All Theaters");
+            //Adds Theater names to the list TheaterNames
+            for (int i = 0; i < BroadwayShow.GetShows().Length; i++)
+            {
+                if (TheaterNames.Contains(BroadwayShow.GetShows()[i].Theater) == false)
+                {
+                    TheaterNames.Add(BroadwayShow.GetShows()[i].Theater);
+                }
+            }
+           // Adds Theatre Names to the ComboBox
+            for (int i = 0; i < TheaterNames.Count; i++)
+            {
+                cbTheatre.Items.Add(TheaterNames[i]);
+            }       
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            //int numbers to help edit row characteristics easier
+            int shows = 0;
+            int num = 3;
+            //Using Excel
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Visible = true;
+
+            Workbook workbook = excelApp.Workbooks.Add();
+            Worksheet sheet = workbook.Worksheets[1];
+
+            // Sets up fornat on Excel such as color,Footer, and text in Excel Boxes
+            sheet.PageSetup.PrintGridlines = true;
+            
+            
+            sheet.PageSetup.LeftFooter= "Jared Bandi "+ dtpStart.Value.ToShortDateString()+"-"+dtpEnd.Value.ToShortDateString();
+
+            DateTime now = DateTime.Now;
+
+            sheet.Cells[1, 1] = "CHI-TOWN BROADWAY "+ now;
+            sheet.Range["A1"].Font.Size = 25;
+
+            sheet.Cells[2, 1] = "SHOW";
+            sheet.Cells[2, 2] = "THEATRE";
+            sheet.Cells[2, 3] = "CLOSING DATE";
+         
+            //Runs through array of shows
+            for (int i = 0; i < BroadwayShow.GetShows().Length; i++)
+            {
+                //Runs through array and checks if selected theater matches at spot[i] or all if it is selected
+                if (cbTheatre.SelectedItem.Equals("All Theaters") || BroadwayShow.GetShows()[i].Theater.Equals(cbTheatre.SelectedItem))
+                {
+                    //Checks that the shows are in the specified range at the specific range
+                    if (BroadwayShow.GetShows()[i].CloseDate > dtpStart.Value && BroadwayShow.GetShows()[i].OpenDate < dtpEnd.Value)
+                    {
+                        sheet.Cells[num, 1] = BroadwayShow.GetShows()[i].Name;
+                        sheet.Cells[num, 2] = BroadwayShow.GetShows()[i].Theater;
+                        sheet.Cells[num, 3] = BroadwayShow.GetShows()[i].CloseDate.ToShortDateString();
+                        num++;
+                        shows++;
+                    }
+                }
+                
+            }
+            //Sets Color of the Body of the Program
+            sheet.Range["A3:A"+num].Font.Color = Color.Aqua;
+            sheet.Range["B3:B"+num].Font.Color = Color.SaddleBrown;
+            sheet.Range["C3:C"+num].Font.Color = Color.PeachPuff;
+
+            //Prints the number of shows
+            sheet.Cells[num, 1] = "There are " + shows + " shows listed";
+            
+           //Autofits the Columns
+            sheet.Columns.AutoFit();
+        }
+    }
+}
